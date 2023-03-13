8c771959680b5b292296a2f6668ed8ee019b0baa
BobrovskaOlena <lena69114@gmail.com>
2023-03-13 23:07:53 +0200

    json

--- MakeJson.java
+++ MakeJson.java
@@ -1,34 +1,39 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 public class MakeJson {
     public static void main(String[] args) throws IOException {
-        String filePath = "file.txt";
+        String filePath = "file2.txt";
         String outputPath = "user.json";
         List<User> users = new ArrayList<>();
-
-        BufferedReader br = new BufferedReader(new FileReader(filePath));
-        String line;
+        FileReader fileReader = new FileReader(filePath);
+        BufferedReader br = new BufferedReader(fileReader);
+        String line = br.readLine();
         boolean firstLine = true;
 
-        while ((line = br.readLine()) != null) {
+        while (line != null){
+            line=br.readLine();
+            if(line==null){
+                break;
+            }
             if (firstLine) {
                 // перший рядок - заголовок
                 firstLine = false;
                 continue;
             }
 
-            String[] field = line.split(" ");
-            String name = field[0];
-            int age = Integer.parseInt(field[1]);
-
+            String[] f = line.split(" ");
+            String name = f[0];
+            int age = Integer.parseInt(f[1]);
             users.add(new User(name, age));
         }
 
         br.close();
 
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         FileWriter writer = new FileWriter(outputPath);
         writer.write(gson.toJson(users));
